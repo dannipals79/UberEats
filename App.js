b6d0584620506cfb206b52b79c5a646276c8ae33
dondanni <36248165+dondanni@users.noreply.github.com>
2022-07-02 18:40:50 +0000

    Uber etas app!!

--- App.js
+++ App.js
@@ -4,7 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      {/* restaurant item   */}
+      <View>
+       <Text> Uber eats app!! </Text>
+
+
+      </View>
+
+
+
+
       <StatusBar style="auto" />
     </View>
   );
